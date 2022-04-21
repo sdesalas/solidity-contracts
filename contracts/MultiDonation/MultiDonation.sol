a995b69b02eb559ce7941d662ee404d4ffa3d4b2
Steven de Salas <steven@desalasworks.com>
2022-04-21 11:02:00 +0200

    Review comments

--- contracts/MultiDonation/MultiDonation.sol
+++ contracts/MultiDonation/MultiDonation.sol
@@ -9,12 +9,9 @@ import "../OpenZepplin/Address.sol";
 
 /**
  * @title MultiDonation
- * @dev This contract allows to split Ether payments among a group of accounts. The sender does not need to be aware
- * that the Ether will be split in this way, since it is handled transparently by the contract.
+ * @dev This contract allows to split Ether or ERC20 payments among a group of accounts each with their relevant share. 
  *
- * The split can be in equal parts or in any other arbitrary proportion. The way this is specified is by assigning each
- * account to a number of shares. Of all the Ether that this contract receives, each account will then be able to claim
- * an amount proportional to the percentage of total shares they were assigned.
+ * The splitting of funds is handled transparently by the contract.
  *
  * `MultiDonation` follows a _pull payment_ model. This means that payments are not automatically forwarded to the
  * accounts but kept in this contract, and the actual transfer is triggered as a separate step by calling the {release}
